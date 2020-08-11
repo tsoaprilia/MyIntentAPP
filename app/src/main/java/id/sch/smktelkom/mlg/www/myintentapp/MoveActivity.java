cce6aaf209b1b2953b956c22480652c5bcf791f8
Aprilia Dwi Cristyana <tsoaprilia@gmail.com>
2020-08-11 13:49:31 +0700

    commit tujuhbelas
    Signed-off-by: Aprilia Dwi Cristyana <tsoaprilia@gmail.com>

---
+++ app/src/main/java/id/sch/smktelkom/mlg/www/myintentapp/MoveActivity.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom.mlg.www.myintentapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move2);
+    }
+}
